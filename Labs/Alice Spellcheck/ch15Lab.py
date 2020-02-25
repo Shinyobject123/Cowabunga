0315052835e95a3b0be339041d2d3c3ae8335a1a
obowers <obowerw020@gmail.com>
2020-02-25 13:45:20 -0600

    Nothing

--- Labs/Alice Spellcheck/ch15Lab.py
+++ Labs/Alice Spellcheck/ch15Lab.py
@@ -8,13 +8,59 @@ Complete the chapter lab at https://docs.google.com/document/d/1KjrNiE3mUbaeyTPp
 
 import re
 
+
 def split_line(line):
     # This function takes in a line of text and returns
     # a list of words in the line.
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
 
 
+with open('./dictionary.txt', 'r') as f:
+    dict = [x.strip().upper() for x in f]
 
+
+def linearSearch():
+    print("--- Linear Search ---")
+
+    file = open('./AliceInWonderLand200.txt', 'r')
+
+    for line in file:
+        words = (split_line(line))
+
+        for i in range(len(words)):
+            word = words[i].upper()
+            j = 0
+            while j < len(dict) and word != dict[j]:
+                j += 1
+            if j >= len(dict):
+                print(word)
+
+    file.close()
+def binarySearch():
+    print("--- Binary Search ---")
+
+    file = open('./AliceInWonderLand200.txt', 'r')
+
+    for line in file:
+        words = (split_line(line))
+
+        for i in range(len(words)):
+            word = words[i].upper()
+            found = False
+            lower_bound = 0
+            upper_bound = len(dict)
+            while lower_bound <= upper_bound and not found:
+                middle_pos = (upper_bound + lower_bound) // 2
+                if dict[middle_pos] < word:
+                    lower_bound = middle_pos + 1
+                elif dict[middle_pos] > word:
+                    upper_bound = middle_pos - 1
+                else:
+                    found = True
+            if not found:
+                    print(word)
+
+linearSearch()
+binarySearch()
 
 # Challenge:  Find all words that occur in Alice through the looking glass that do NOT occur in Wonderland.
-
